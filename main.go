11c925e4b3295ca6fce9ef17b2dd016421f401c2
shranaik <shranaik@cisco.com>
2017-03-15 14:23:45 -0700

    Adding login template

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -21,9 +22,24 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request Method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+
+}
+
 func main() {
-	http.HandleFunc("/", sayHelloName)       //set router
-	err := http.ListenAndServe(":9090", nil) //set listening port
+	http.HandleFunc("/", sayHelloName) //set router
+	http.HandleFunc("/login", login)
+	// nil --> DefaultServeMux, router variable which can cal handler functions fo specified URLs
+	err := http.ListenAndServe(":9090", nil) //set listening port and initialize a server object
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
